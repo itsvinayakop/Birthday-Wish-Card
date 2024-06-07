11ff26236cd487bdae05f50d99839b6f174078dd
Vinayak Bansal <94393925+itsvinayakop@users.noreply.github.com>
2024-06-08 00:20:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = "Dear topper dost,";
+
+desc.innerText = "Happy Birthday to you! Your kindness, support, and beautiful spirit light up my life. Hope your day is as wonderful as you are! 🎉💕.";
+
+sname.innerText = "Vinayak Bansal";
